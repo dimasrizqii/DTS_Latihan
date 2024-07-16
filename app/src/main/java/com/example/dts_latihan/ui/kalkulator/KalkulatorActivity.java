01c0f4101b74cb8783f12bee90ca9611a182f262
Dimas Rizqi Ibadurrahim <akbarabi72@gmail.com>
2024-07-16 09:15:59 +0700

    Function Tugas 1

--- app/src/main/java/com/example/dts_latihan/ui/kalkulator/KalkulatorActivity.java
+++ app/src/main/java/com/example/dts_latihan/ui/kalkulator/KalkulatorActivity.java
@@ -1,8 +1,11 @@
 package com.example.dts_latihan.ui.kalkulator;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +19,8 @@ public class KalkulatorActivity extends AppCompatActivity {
 
     ActivityKalkulatorBinding binding;
 
+    int a, b;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,10 +30,101 @@ public class KalkulatorActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        binding.btnTambah.setOnClickListener(new View.OnClickListener() {
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                EditText number1 = binding.etAngkaPertama;
+                EditText number2 = binding.etAngkaKedua;
+                if (number1.getText().toString().trim().isEmpty() && number2.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Masukkan angka terlebih dahulu", Toast.LENGTH_SHORT).show();
+                } else if (number1.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Masukkan angka pertama", Toast.LENGTH_SHORT).show();
+                }  else if (number2.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Masukkan angka kedua", Toast.LENGTH_SHORT).show();
+                } else {
+                    a = Integer.parseInt(number1.getText().toString());
+                    b = Integer.parseInt(number2.getText().toString());
+                    binding.tvHasilAngka.setText("" + (a+b));
+                }
+            }
+        });
+
+        binding.btnKurang.setOnClickListener(new View.OnClickListener() {
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                EditText number1 = binding.etAngkaPertama;
+                EditText number2 = binding.etAngkaKedua;
+                if (number1.getText().toString().trim().isEmpty() && number2.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Masukkan angka terlebih dahulu", Toast.LENGTH_SHORT).show();
+                } else if (number1.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Masukkan angka pertama", Toast.LENGTH_SHORT).show();
+                }  else if (number2.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Masukkan angka kedua", Toast.LENGTH_SHORT).show();
+                } else {
+                    a = Integer.parseInt(number1.getText().toString());
+                    b = Integer.parseInt(number2.getText().toString());
+                    binding.tvHasilAngka.setText("" + (a-b));
+                }
+            }
+        });
+
+        binding.btnKali.setOnClickListener(new View.OnClickListener() {
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                EditText number1 = binding.etAngkaPertama;
+                EditText number2 = binding.etAngkaKedua;
+                if (number1.getText().toString().trim().isEmpty() && number2.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Masukkan angka terlebih dahulu", Toast.LENGTH_SHORT).show();
+                } else if (number1.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Masukkan angka pertama", Toast.LENGTH_SHORT).show();
+                }  else if (number2.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Masukkan angka kedua", Toast.LENGTH_SHORT).show();
+                } else {
+                    a = Integer.parseInt(number1.getText().toString());
+                    b = Integer.parseInt(number2.getText().toString());
+                    binding.tvHasilAngka.setText("" + a*b);
+                }
+            }
+        });
+
+        binding.btnBagi.setOnClickListener(new View.OnClickListener() {
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                EditText number1 = binding.etAngkaPertama;
+                EditText number2 = binding.etAngkaKedua;
+                if (number1.getText().toString().trim().isEmpty() && number2.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Masukkan angka terlebih dahulu", Toast.LENGTH_SHORT).show();
+                } else if (number1.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Masukkan angka pertama", Toast.LENGTH_SHORT).show();
+                }  else if (number2.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Masukkan angka kedua", Toast.LENGTH_SHORT).show();
+                } else {
+                    a = Integer.parseInt(number1.getText().toString());
+                    b = Integer.parseInt(number2.getText().toString());
+                    binding.tvHasilAngka.setText("" + a/b);
+                }
+            }
+        });
+        
+        binding.btnBersihkan.setOnClickListener(this::resetNumber);
+
         binding.btnBack.setOnClickListener(this::goBack);
 
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
     }
+
+    private void resetNumber(View view) {
+        binding.tvHasilAngka.setText("0");
+    }
+
 
     public void goBack(View view) {
         Intent intent = new Intent(KalkulatorActivity.this, MainActivity.class);
--- app/src/main/java/com/example/dts_latihan/ui/listview/ListViewActivity.java
+++ app/src/main/java/com/example/dts_latihan/ui/listview/ListViewActivity.java
@@ -2,8 +2,10 @@ package com.example.dts_latihan.ui.listview;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -31,5 +33,25 @@ public class ListViewActivity extends AppCompatActivity {
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, country);
         listview.setAdapter(arrayAdapter);
+
+        listview.setOnItemClickListener((adapterView, view1, i, l) -> {
+            if(i==0){
+                Toast.makeText(getApplicationContext(), "Indonesia", Toast.LENGTH_SHORT).show();
+            } else if (i==1) {
+                Toast.makeText(getApplicationContext(), "Malaysia", Toast.LENGTH_SHORT).show();
+            } else if (i==2) {
+                Toast.makeText(getApplicationContext(), "Philippines", Toast.LENGTH_SHORT).show();
+            } else if (i==3) {
+                Toast.makeText(getApplicationContext(), "Myanmar", Toast.LENGTH_SHORT).show();
+            } else if (i==4) {
+                Toast.makeText(getApplicationContext(), "Vietnam", Toast.LENGTH_SHORT).show();
+            } else if (i==5) {
+                Toast.makeText(getApplicationContext(), "Brunei Darussalam", Toast.LENGTH_SHORT).show();
+            } else if (i==6) {
+                Toast.makeText(getApplicationContext(), "Cambodia", Toast.LENGTH_SHORT).show();
+            } else if (i==7) {
+                Toast.makeText(getApplicationContext(), "Laos", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
